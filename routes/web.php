5abc01afeb4977f3b5f982c227a03f30ffacc543
Nadhiradhit <nadhiradhitya@gmail.com>
2023-11-13 10:30:11 +0700

    feat(user): first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('vendor');
+    return view('welcome');
 });
 Route::get('/user', function () {
     return view('user');
@@ -25,3 +26,6 @@ Route::get('/status', function () {
 Route::get('/order', function () {
     return view('order');
 });
+
+// Route Auth
+Route::get('/login', [AuthController::class, 'index'])->name('auth.login');
