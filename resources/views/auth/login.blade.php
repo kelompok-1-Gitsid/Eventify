6ac7ddbadfb500e31917fbf439c7e5dd5f0de186
Nadhiradhit <nadhiradhitya@gmail.com>
2023-11-13 22:22:30 +0700

    feat(user): overview login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,52 @@
 @extends('layouts.auth')
 
 @section('container')
-<div class="w-full max-h-screen px-[165px] py-[70px] bg-white justify-center items-center inline-flex">
-    <div class="w-[950px] h-[580px] relative">
+<div class="w-full h-screen bg-white justify-center items-center inline-flex">
+    {{-- Md until Xl --}}
+    <div class="w-1/2 h-[580px] relative md:block hidden">
         <div class="w-[950px] h-[580px] left-0 top-0 absolute bg-slate-50 rounded-[13px] shadow">
             <div class="text-zinc-800 ">
-                <h1 class="mt-2 text-[22px] text-center font-bold font-['Plus Jakarta Sans']">Welcome back</h1>
-                <h2 class="mt-1 text-[15px] text-center font-medium font-['Plus Jakarta Sans']">We are so happy, u come back</h2>    
+                <h1 class="mt-5 text-[24px] text-center font-bold font-plusJakarta">Welcome back</h1>
+                <h2 class="mt-1 text-[15px] text-center font-medium font-plusJakarta">We are so happy, u come back</h2>    
+            </div>
+            <div class="container flex flex-row mt-4">
+                <div class="basis-1/2">
+                    <form action="" method="post">
+                        <div class="mt-4 ml-6 font-plusJakarta">
+                            <label for="">Email or Username</label>
+                            <div class="flex w-[24rem] mt-2 rounded-md shadow-sm ring-1 ring-inset ring-gray-300">
+                                <input type="text" name="" id="" class="block flex-1 border-0 bg-transparent py-1.5 pl-2.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+                        <div class="mt-4 ml-6 font-plusJakarta">
+                            <label for="">Password</label>
+                            <div class="flex w-[24rem] mt-2 rounded-md shadow-sm ring-1 ring-inset ring-gray-300">
+                                <input type="password" name="" id="" class="block flex-1 border-0 bg-transparent py-1.5 pl-2.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+                        <div class="mt-4 mr-16 font-plusJakarta flex flex-row-reverse">
+                            <a href="" class="text-bluelight hover:text-blue-700">Forgot Password?</a>
+                        </div>
+                        <div class="mt-4 ml-6 font-plusJakarta">
+                            <button class="w-[24rem] py-2.5 text-center bg-cyan rounded-md shadow-sm hover:bg-bluelight hover:text-white">Login</button>
+                            <div class="flex mt-2">
+                                <p>Need Account?<a href="" class="text-bluelight hover:text-blue-700 ml-2">Register</a></p>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="ml-6 mt-4 w-[24rem] font-plusJakarta">
+                        <p class="text-center">Or</p>
+                        <button type="button" class="text-gray-900 w-full bg-white mt-4 hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 48 48" class="w-6 h-6 me-2 -ms-1">
+                                <path fill="#fbc02d" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#e53935" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4caf50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1565c0" d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+                            </svg>
+                            Connect with Google
+                        </button>
+                    </div>
+                </div>
+                <div class="basis-1/2">
+                   
+                </div>
             </div>
         </div>
         {{-- 
