50730715f08baf9df6beac6dd0196af2ac31e4d8
Nadhiradhit <nadhiradhitya@gmail.com>
2023-11-21 12:43:44 +0700

    feat: auth login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+route::get('/home',[HomeController::class,'index'])->middleware('auth')->name('home');
 
 require __DIR__.'/auth.php';
