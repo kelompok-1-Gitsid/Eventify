98582f270dc4df5d33ea2c12a4ab1871eaacc855
Nadhiradhit <nadhiradhitya@gmail.com>
2023-11-23 12:43:47 +0700

    feat(database) : database migration

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Category::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $data = [
+            ["name" => "Wedding"],
+            ["name" => "Catering"],
+            ["name" => "Videographer"],
+            ["name" => "Makeup Artist"],
+        ];
+
+        foreach ($data as $value){
+            Category::insert([
+                "name" => $value["name"],
+                "created_at" => Carbon::now(),
+                "updated_at" => Carbon::now()
+            ]);
+        }
+    }
+}
