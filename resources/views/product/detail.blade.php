8588f13418ad72aee87d2de9a93f714b25806122
pyyyy1 <japicobaaja@gmail.com>
2023-11-27 19:22:56 +0700

    fix: fix button order

--- resources/views/product/detail.blade.php
+++ resources/views/product/detail.blade.php
@@ -24,10 +24,10 @@
                         </a>
 
                         <!-- Order Button -->
-                        <button
-                            class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-green-500 rounded-lg hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                        <a href="{{ url('/') }}"
+                        class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Order Now
-                        </button>
+                        </a>
                     </div>
                 </div>
             </div>
