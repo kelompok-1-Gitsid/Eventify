69d10dcc253989c4cd89c0eccbe3822e40dffc32
Nadhiradhit <nadhiradhitya@gmail.com>
2023-11-27 18:06:00 +0700

    fix(register) : delete signup with google

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,43 +65,6 @@
                 </x-primary-button>
             </div>
 
-
-            <div class="">
-                <div class="font-plusJakarta">
-                    <div class="flex items-center justify-between mx-2 my-5">
-                        <hr class="border-b border-gray-500 w-52">
-                        <p class=" ">or</p>
-                        <hr class="border-b border-gray-500 w-52">
-                    </div>
-                    <button type="button"
-                        class="text-gray-800 w-full bg-white mt-4 hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center me-2 mb-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 48 48"
-                            class="w-6 h-6 me-7 -ms-7">
-                            <path fill="#fbc02d"
-                                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z">
-                            </path>
-                            <path fill="#e53935"
-                                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z">
-                            </path>
-                            <path fill="#4caf50"
-                                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z">
-                            </path>
-                            <path fill="#1565c0"
-                                d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z">
-                            </path>
-                        </svg>
-                        Sign up with Google
-                    </button>
-                    <div class="flex justify-center mt-4">
-                        <p class=""> have an account ?
-                            <a class="ml-1 underline text-sm text-bluelight hover:text-gray-900 rounded-md"
-                                href="{{ route('login') }}">
-                                {{ __('Already registered?') }}
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
         </form>
     </div>
 
