08febca91b5536ddcc32a4a97d3fe987d3f84317
Nadhiradhit <nadhiradhitya@gmail.com>
2023-11-29 22:31:57 +0700

    fix(user): fix bug middleware

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,8 +11,23 @@
                     </p>
                 </div>
                 <div class="image top-0">
-                    <img src="{{ url('assets/img/wedding.jpg') }}" />
+                    <div>
+                        <img src="{{ url('assets/img/wedding-header.jpg') }}" />
+                    </div>
                 </div>
+            </div>
+        </div>
+    </section>
+    <section id="strategy" class=" bg-slate-100">
+        <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+            <div class="font-light text-gray-500 sm:text-lg">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">We didn't reinvent the wheel</h2>
+                <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+                <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+            </div>
+            <div class="grid grid-cols-2 gap-4 mt-8">
+                <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1">
+                <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2">
             </div>
         </div>
     </section>
@@ -79,10 +94,6 @@
                         <img class="h-auto max-w-full rounded-lg"
                             src="{{ url('assets/img/video-product.jpg')}}" alt="">
                     </div>
-                    <div>
-                        <img class="h-auto max-w-full rounded-lg"
-                            src="{{ url('assets/img/photo-product.jpg')}}" alt="">
-                    </div>
                 </div>
                 <div class="grid gap-4">
                     <div>
@@ -92,10 +103,6 @@
                     <div>
                         <img class="h-auto max-w-full rounded-lg"
                             src="{{ url('assets/img/photo-product-2.jpg')}}" alt="">
-                    </div>
-                    <div>
-                        <img class="h-auto max-w-full rounded-lg"
-                            src="{{ url('assets/img/wedding-product.jpg')}}" alt="">
                     </div>
                 </div>
                 <div class="grid gap-4">
@@ -107,16 +114,14 @@
                         <img class="max-h-screen max-w-full rounded-lg"
                             src="{{ url('assets/img/makeup-product-2.jpg')}}" alt="">
                     </div>
-                    <div>
-                        <img class="h-auto max-w-full rounded-lg"
-                            src="{{ url('assets/img/dummy.jpg')}}" alt="">
-                    </div>
                 </div>
             </div>
         </div>
     </section>
     <section id="testimony">
-        <div class="w-full"></div>
+        <div class="w-full">
+
+        </div>
     </section>
 
 </x-user-layout>
