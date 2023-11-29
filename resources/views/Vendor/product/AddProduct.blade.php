fa1118024e7b8989d007e0a0b051c9b28f9fe8c0
Nadhiradhit <nadhiradhitya@gmail.com>
2023-11-29 22:32:31 +0700

    feat(middleware) : add middleware for order

--- resources/views/Vendor/product/AddProduct.blade.php
+++ resources/views/Vendor/product/AddProduct.blade.php
@@ -17,7 +17,7 @@
                 </div>
                 <div class="form-group">
                     <label for="price">Price</label>
-                    <input type="number" name="price" id="price" step="0.01" class="form-control" required>
+                    <input type="text" name="price" id="price" step="0.01" class="form-control" required>
                 </div>
                 <div class="form-group">
                     <label for="image1">Image 1</label>
