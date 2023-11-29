ed6e5b0936b6319dbd47aac01ebe239b6263cdc2
Nadhiradhit <nadhiradhitya@gmail.com>
2023-11-29 10:53:14 +0700

    feat(user): add galery product

--- resources/views/components/footer-user.blade.php
+++ resources/views/components/footer-user.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-gray-900 font-plusJakarta w-full z-20">
+<footer class="bg-gray-900 font-plusJakarta w-full z-20 fixed-bottom">
     <div class="w-full p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between m-5">
             <a href="{{ url('/') }}" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,24 +16,22 @@
             </div>
         </div>
     </section>
-    <section id="our-product" class="mb-10 mt-10">
+    <section id="product" class="mb-10 mt-10">
         <div class="py-2 text-center font-plusJakarta">
             <h1 class="font-bold text-[1.5rem] md:text-[2rem] lg:text-[2.5rem]"> Our Product</h1>
             <h3 class="font-medium text-[1rem] md:text-[1.2rem] lg:text-[1.4rem]">This are our priority product</h3>
         </div>
         <div class="mt-10 mb-10 mx-8 flex justify-items-center items-center justify-center ">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10 md:gap-20 min-h-full">
-                <div
-                    class="relative scale">
+                <div class="relative scale">
                     <div class="image-product">
                         <img src="{{ url('assets/img/wedding-product.jpg') }}" alt="" class="rounded-lg">
                     </div>
                     <div class="absolute top-0 left-0 right-0 bottom-0 mb-2 ml-2 flex items-end justify-start">
-                        <p class="text-white text-2xl font-bold">Wedding</p>
+                        <p class="text-white text-2xl font-bold">Decoration</p>
                     </div>
                 </div>
-                <div
-                    class="relative scale">
+                <div class="relative scale">
                     <div class="image-product">
                         <img src="{{ url('assets/img/photo-product.jpg') }}" alt="" class="rounded-lg">
                     </div>
@@ -41,8 +39,7 @@
                         <p class="text-white text-2xl font-bold">Photography</p>
                     </div>
                 </div>
-                <div
-                    class="relative scale">
+                <div class="relative scale">
                     <div class="image-product">
                         <img src="{{ url('assets/img/video-product.jpg') }}" alt="" class="rounded-lg">
                     </div>
@@ -50,8 +47,7 @@
                         <p class="text-white text-2xl font-bold">Videographer</p>
                     </div>
                 </div>
-                <div
-                    class="relative scale">
+                <div class="relative scale">
                     <div class="image-product ">
                         <img src="{{ url('assets/img/catering-product.jpg') }}" alt="" class="rounded-lg">
                     </div>
@@ -59,8 +55,7 @@
                         <p class="text-white text-2xl font-bold">Catering</p>
                     </div>
                 </div>
-                <div
-                    class="relative scale">
+                <div class="relative scale">
                     <div class="image-product">
                         <img src="{{ url('assets/img/makeup-product.jpg') }}" alt="" class="rounded-lg">
                     </div>
@@ -71,50 +66,57 @@
             </div>
         </div>
     </section>
-    <section id="content" class="mb-10 mt-10">
-    <div class="flex flex-col mx-auto">
-        <h1 class="text-3xl font-semibold text-center my-16">Top Vendor</h1>
-        <div class="flex flex-col lg:flex-row items-center md:justify-center lg:gap-20">
-            <div class="mx-5 my-5 image">
-                <img src="{{ url('assets/img/dummy.jpg') }}" alt="" class="rounded-lg">
-            </div>
-            <div class="mx-5 my-5 font-plusJakarta text-center md:my-4 lg:my-auto lg:w-[50rem] lg:text-justify">
-                <h1 class="title text-[1.5rem]">MegaWedding Entertaiment</h1>
-                <p class="mt-5 text-justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eum aspernatur
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit molestias laudantium itaque
-                    dolores corporis, labore expedita in obcaecati, cumque doloremque ut pariatur culpa. Iure ullam
-                    aliquid dolorem a iste facere.
-                </p>
-                <div class="flex flex-row items-center mt-8">
-                    <button class="bg-blue-950 w-32 p-2 rounded-md scale">
-                        <a href="" class="text-white">See Details</a>
-                    </button>
-                    <p class="ml-8">start at <span class="font-bold text-xl">Rp.750.000</span>/day</p>
+    <section id="galery" class="mb-10 mt-24">
+        <div class="w-3/4 h-auto bg-slate-100 flex flex-col mx-auto rounded-lg">
+            <h1 class="text-4xl font-semibold text-center my-16">Galery</h1>
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-4 px-16 pb-12">
+                <div class="grid gap-4">
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="{{ url('assets/img/potrait.jpg')}}" alt="">
+                    </div>
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="{{ url('assets/img/video-product.jpg')}}" alt="">
+                    </div>
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="{{ url('assets/img/photo-product.jpg')}}" alt="">
+                    </div>
+                </div>
+                <div class="grid gap-4">
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="{{ url('assets/img/catering-product.jpg')}}" alt="">
+                    </div>
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="{{ url('assets/img/photo-product-2.jpg')}}" alt="">
+                    </div>
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="{{ url('assets/img/wedding-product.jpg')}}" alt="">
+                    </div>
+                </div>
+                <div class="grid gap-4">
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="{{ url('assets/img/dummy.jpg')}}" alt="">
+                    </div>
+                    <div>
+                        <img class="max-h-screen max-w-full rounded-lg"
+                            src="{{ url('assets/img/makeup-product-2.jpg')}}" alt="">
+                    </div>
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="{{ url('assets/img/dummy.jpg')}}" alt="">
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="flex flex-col lg:flex-row-reverse items-center md:justify-center lg:gap-20">
-            <div class="mx-5 my-5 image">
-                <img src="{{ url('assets/img/dummy.jpg') }}" alt="" class="rounded-lg">
-            </div>
-            <div class="mx-5 my-5 font-plusJakarta text-center md:my-4 lg:my-auto lg:w-[50rem] lg:text-justify">
-                <h1 class="title text-[1.5rem]">Ichibano Studio</h1>
-                <p class="mt-5 text-justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eum aspernatur
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit molestias laudantium itaque
-                    dolores corporis, labore expedita in obcaecati, cumque doloremque ut pariatur culpa. Iure ullam
-                    aliquid dolorem a iste facere.
-                </p>
-                <div class="flex flex-row items-center mt-8">
-                    <button class="bg-blue-950 w-32 p-2 rounded-md scale">
-                        <a href="" class="text-white">See Details</a>
-                    </button>
-                    <p class="ml-8">start at <span class="font-bold text-xl">Rp.1.750.000</span>/day</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    </section>
+    <section id="testimony">
+        <div class="w-full"></div>
     </section>
 
 </x-user-layout>
