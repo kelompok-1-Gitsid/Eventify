e5e6400dec24fbe29ecabe259c962f73b0d7ecb9
Nadhiradhit <nadhiradhitya@gmail.com>
2023-12-01 00:20:41 +0700

    feat : add animation

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -54,6 +54,12 @@
                 <x-input-error class="mt-2" :messages="$errors->get('address')" />
             </div>
 
+            <div>
+                <x-input-label for="phone" :value="__('Phone')" />
+                <x-text-input id="phone" name="phone" type="text" class="mt-1 block w-full" :value="old('phone', $user->phone)" required autofocus autocomplete="name" />
+                <x-input-error class="mt-2" :messages="$errors->get('phone')" />
+            </div>
+
             <div class="flex items-center gap-4">
                 <x-primary-button class="submit-button flex w-52 justify-center rounded-md">
                     {{ __('Save') }}
