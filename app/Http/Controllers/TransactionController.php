4d27a60cccd75f5243263de54be63b85f78a5462
Nadhiradhit <nadhiradhitya@gmail.com>
2023-12-01 00:27:04 +0700

    refractor: clean code

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TransactionController extends Controller
+{
+    public function index(){
+        return view('admin.transaction');
+    }
+    public function store(Request $request)
+    {
+        // Validate request data
+        $validator = Validator::make($request->all(), [
+            'product_id' => 'required|exists:products,id',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'price'     => 'required'
+        ]);
+
+        // Check for validation errors
+
+        // if ($validator->fails()) {
+        //     dd($request->all());
+        // }
+
+        try {
+            $data = $request->all();
+            $user = $request->user();
+            $data['user_id'] = $user->id;
+
+            // Create a new Order instance
+            // dd($data);
+            $order = Transaction::create($data);
+
+            // Redirect to product page after successful order creation
+            return redirect()->route('product.detail', ['id' => $request->productName])->with('success', 'Order created successfully');
+        } catch (\Exception $e) {
+            // Handle exception
+            return redirect()->back()->with('failed', 'Order created failed. Error: ' . $e->getMessage())->withInput();
+        }
+    }
+}
