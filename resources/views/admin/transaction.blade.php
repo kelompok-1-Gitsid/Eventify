65915719f8c4678aa57abcb87c65e39d992c1be5
musangking15 <haikalfajari156@gmail.com>
2023-12-01 16:13:10 +0700

    Fix : route infocontroller and admin (transaction)

--- resources/views/admin/transaction.blade.php
+++ resources/views/admin/transaction.blade.php
@@ -38,46 +38,21 @@
                                 </tr>
                             </thead>
                             <tbody class="text-center">
-                                <tr>
-                                    <td>Adelia</td>
-                                    <td>Amira Catering</td>
-                                    <td>15-10-23</td>
-                                    <td>05-10-23</td>
-                                    <td>Catering</td>
-                                    <td><button class="btn btn-primary" disabled>Paid</button></td>
-                                </tr>
-                                <tr>
-                                    <td>Adelia</td>
-                                    <td>Ruang Reka</td>
-                                    <td>15-10-23</td>
-                                    <td>05-10-23</td>
-                                    <td>Videographer</td>
-                                    <td><button class="btn btn-primary" disabled>Paid</button></td>
-                                </tr>
-                                <tr>
-                                    <td>Adelia</td>
-                                    <td>Wilhelmina Wedding</td>
-                                    <td>15-01-24</td>
-                                    <td>-</td>
-                                    <td>Decoration</td>
-                                    <td><button class="btn btn-danger" disabled>Unpaid</button></td>
-                                </tr>
-                                <tr>
-                                    <td>Adelia</td>
-                                    <td>Adelia MUA</td>
-                                    <td>20-01-24</td>
-                                    <td>-</td>
-                                    <td>MUA</td>
-                                    <td><button class="btn btn-danger" disabled>Unpaid</button></td>
-                                </tr>
-                                <tr>
-                                    <td>Adelia</td>
-                                    <td>Red Circle photography</td>
-                                    <td>25-01-24</td>
-                                    <td>-</td>
-                                    <td>Photographer</td>
-                                    <td><button class="btn btn-danger" disabled>Unpaid</button></td>
-                                </tr>
+                                @foreach ($transaction as $row)
+                                    <tr>
+                                        <td>{{ $row->user->name }}</td>
+                                        <td>{{ $row->product->name }}</td>
+                                        <td>{{ $row->start_date }} - {{ $row->end_date }} </td>
+                                        <td>-</td>
+                                        <td>{{ $row->product->category }}</td>
+                                        <td>
+                                            {!! $row->status == 'Paid'
+                                                ? '<button class="btn btn-success" disabled>' . $row->status . '</button>'
+                                                : '<button class="btn btn-primary" disabled>' . $row->status . '</button>' !!}
+                                        </td>
+
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
