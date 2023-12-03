3457a14fa454eaf30e933391525324e64600c0c0
Rinaldi2212 <rinaldisimbolon9@gmail.com>
2023-12-03 07:34:15 +0700

    Fix: View Product

--- resources/views/Vendor/product/MyProduct.blade.php
+++ resources/views/Vendor/product/MyProduct.blade.php
@@ -2,63 +2,51 @@
 
 @section('content')
 
-    <section class="intro mt-5">
-        <div class="mask d-flex align-items-center h-100" style="background-color: rgba(0,0,0,0);">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-12">
-                        <div class="card bg-dark shadow-2-strong">
-                            <div class="card-body">
-                                @if($product)
-                                    <div class="table-responsive">
-                                        <table class="table table-dark table-borderless mb-0 table-striped">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col">Name</th>
-                                                    <th scope="col">Description</th>
-                                                    <th scope="col">Price</th>
-                                                    <th scope="col">Category</th>
-                                                    <th scope="col">Image 1</th>
-                                                    <th scope="col">Image 2</th>
-                                                    <th scope="col">Image 3</th>
-                                                    <th scope="col">Image 4</th>
-                                                    <th scope="col">Image 5</th>
-                                                    <th scope="col">Action</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>{{ $product->name }}</td>
-                                                    <td>{{ $product->description }}</td>
-                                                    <td>Rp {{ $product->price }}</td>
-                                                    <td>{{ $product->category }}</td>
-                                                    <td><img src="{{ asset($product->image1) }}" alt="{{ $product->name }}" class="img-fluid" /></td>
-                                                    <td><img src="{{ asset($product->image2) }}" alt="{{ $product->name }}" class="img-fluid" /></td>
-                                                    <td><img src="{{ asset($product->image3) }}" alt="{{ $product->name }}" class="img-fluid" /></td>
-                                                    <td><img src="{{ asset($product->image4) }}" alt="{{ $product->name }}" class="img-fluid" /></td>
-                                                    <td><img src="{{ asset($product->image5) }}" alt="{{ $product->name }}" class="img-fluid" /></td>
-                                                    <td>
-                                                        <a href="{{ route('product.edit', $product->id) }}" class="btn btn-primary">Edit</a>
-                                                        <form action="{{ route('product.destroy', $product->id) }}" method="POST" class="d-inline">
-                                                            @csrf
-                                                            <!-- Metode DELETE -->
-                                                            @method('DELETE')
-                                                            <button type="submit" class="btn btn-danger">Delete</button>
-                                                        </form>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                @else
-                                    <p class="text-center">No product available.</p>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<div class="flex justify-center items-center h-screen mt-5">
+    <div class="w-full max-w-4xl my-24 px-8">
+        <!-- Product Card -->
+        <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+            <!-- Product Images -->
+            <div class="flex overflow-x-auto gap-4 text-center p-4">
+                <img src="{{ asset($product->image1) }}" style="width: 15%; max-height: 200px; object-fit: cover;" class="rounded-lg inline-block m-2" alt="{{ $product->name }}">
+                <img src="{{ asset($product->image2) }}" style="width: 15%; max-height: 200px; object-fit: cover;" class="rounded-lg inline-block m-2" alt="{{ $product->name }}">
+                <img src="{{ asset($product->image3) }}" style="width: 15%; max-height: 200px; object-fit: cover;" class="rounded-lg inline-block m-2" alt="{{ $product->name }}">
+                <img src="{{ asset($product->image4) }}" style="width: 15%; max-height: 200px; object-fit: cover;" class="rounded-lg inline-block m-2" alt="{{ $product->name }}">
+                <img src="{{ asset($product->image5) }}" style="width: 15%; max-height: 200px; object-fit: cover;" class="rounded-lg inline-block m-2" alt="{{ $product->name }}">
+            </div>
+
+            <!-- Product Details -->
+            <div class="p-4 text-center"> <!-- Menambahkan kelas text-center di sini -->
+                <h1 class="text-4xl">{{ $product->name }}</h1>
+                <p class="text-gray-600 text-base">{{ $product->description }}</p>
+                <p class="text-2xl font-bold mt-4">Rp.{{ $product->price }}</p>
+
+          <!-- Buttons -->
+          <div class="flex items-center gap-4 mt-8 px-8">
+            <!-- Edit Button -->
+            <form action="{{ route('product.edit', $product->id) }}" method="GET">
+                @csrf
+                <button type="submit" style="background-color: #3490dc; color: #fff; padding: 8px 16px; border-radius: 8px; cursor: pointer;">
+                    Edit
+                </button>
+            </form>
+
+            <!-- Delete Button -->
+            <form action="{{ route('product.destroy', $product->id) }}" method="POST">
+                @csrf
+                <!-- Metode DELETE -->
+                @method('DELETE')
+                <button type="submit" class="mt-1" style="background-color: #e3342f; color: #fff; padding: 8px 16px; border-radius: 8px; cursor: pointer;">
+                    Delete
+                </button>
+            </form>
+        </div>
+
+
             </div>
         </div>
-    </section>
+        <!-- End Product Card -->
+    </div>
+</div>
 
 @endsection
