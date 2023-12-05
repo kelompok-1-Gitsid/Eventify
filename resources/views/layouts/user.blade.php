ad0facddb70440234fbf76a26792ada5d19f104e
Nadhiradhit <nadhiradhitya@gmail.com>
2023-12-05 08:07:48 +0700

    feat: add icon

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ url('assets/img/logo2.jpg')}}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{env('MIDTRANS_CLIENT_KEY')}}"></script>
